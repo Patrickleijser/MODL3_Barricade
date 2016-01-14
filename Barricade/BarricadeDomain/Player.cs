fb4affc97d3442e548ab8e2cace3829cce6dae5f
patrick@breaky.eu <rsVmy123>
2016-01-14 17:37:23 +0100

    Basic Classes created

---
+++ Barricade/BarricadeDomain/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarricadeDomain
+{
+    class Player
+    {
+        public String Name { get; private set; }
+        public String Color { get; set; }
+        public List<Pawn> Pawns { get; set; }
+
+        public IField StartField { get; set; }
+
+        public Player(String name)
+        {
+            Name = name;
+        }
+    }
+}
